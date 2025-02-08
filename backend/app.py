6c66f6ed8125055460f2aff0ec7b0fb25380141b
Liam Baird <liambaird628@gmail.com>
2025-02-08 15:16:12 -0500

    remove pull_usernames

--- backend/app.py
+++ backend/app.py
@@ -62,12 +62,6 @@ class CommentForm(FlaskForm):
     submit = SubmitField("Post Comment")
 
 
-@app.route("/", methods=["GET", "POST"])
-@app.route("/test", methods=["GET", "POST"])
-def pull_usernames():
-    return db.session.query(User).all()
-
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
