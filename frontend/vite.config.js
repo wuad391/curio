50a49e7924fa40abfeb81cf7285d1e26c219c3e1
wuad391 <wuad391@gmail.com>
2025-02-08 01:46:09 -0500

    TRY AGAIN

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/test": {
-        target: "https://127.0.0.1:5000"
+        target: "https://127.0.0.1:5000/test"
       }
     }
   }
