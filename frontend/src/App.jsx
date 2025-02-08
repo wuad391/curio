04c39d4479a5e0bb6449b53e47c4326dc542ad37
wuad391 <wuad391@gmail.com>
2025-02-08 01:38:42 -0500

    another stupid error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -189,7 +189,7 @@ const App = () => {
     <div className="flex h-screen">
       {/* Side Navigation */}
       <SideNav />
-      <div>{ message }</div>
+      <div>{ test }</div>
       {/* Main Content Area */}
       <div className="flex-1 flex flex-col">
         {/* Search Bar at the Top */}
