cbb68c13d135ea415a5d9293b6454a53cc8e1711
wuad391 <wuad391@gmail.com>
2025-02-08 01:37:24 -0500

    added useeffect :(

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Home, MessageSquare, Search, Star, Book, Plus, MessageCircle } from "lucide-react";
 
 
