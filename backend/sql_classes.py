f7b8d09c58c7b0f0f71f30ccbb27780e1ca60b73
Liam Baird <liambaird628@gmail.com>
2025-02-08 13:35:11 -0500

    added testing example and update useroles to Enum

--- backend/sql_classes.py
+++ backend/sql_classes.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import secrets
+from roles import UserRoles
 
 app = Flask(__name__)
 app.secret_key = secrets.token_urlsafe(16)
@@ -15,21 +16,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    role = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.Enum(UserRoles), nullable=False)
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False)
-    user_role = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     content = db.Column(db.Text, nullable=False)
     comments = db.relationship("Comment", backref="post", lazy=True)
 
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(150), nullable=False)
-    user_role = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     content = db.Column(db.Text, nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
     instructor_endorsed = db.Column(db.Boolean, default=False)
--- backend/tester.py
+++ backend/tester.py
@@ -1,11 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import secrets
+from sql_classes import *
+from roles import UserRoles
+from scoring import (
+    Ranking,
+    StarHistory,
+    add_to_history,
+    derived_post_score,
+    derived_post_stars,
+    post_visibility,
+    derived_comment_score,
+)
 
-app = Flask(__name__)
-app.secret_key = secrets.token_urlsafe(16)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///curio.db"
-db = SQLAlchemy(app)
+# create example users, posts, and comments
 with app.app_context():
     db.create_all()
-app.run(debug=True)
+    user1 = User(username="user1", password="password", role=UserRoles.STUDENT)
+    user2 = User(username="user2", password="password", role=UserRoles.STUDENT)
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.commit()
+    post1 = Post(user_id=user1.id, content="post1 content")
+    post2 = Post(user_id=user2.id, content="post2 content")
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.commit()
+    comment1 = Comment(user_id=user1.id, content="comment1 content", post_id=post2.id)
+    comment2 = Comment(user_id=user2.id, content="comment2 content", post_id=post1.id)
+    instructor1 = User(
+        username="instructor1", password="password", role=UserRoles.INSTRUCTOR
+    )
